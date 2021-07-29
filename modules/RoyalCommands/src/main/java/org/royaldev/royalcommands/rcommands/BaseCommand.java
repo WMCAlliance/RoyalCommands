8265dab340eb2d7ad1b7b1f50be96ea5c7f64633
Nathan Blaney <nathan@blaney.xyz>
2021-07-29 22:51:16 +1000

    Fixes tab complete broken in last commit

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/BaseCommand.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/BaseCommand.java
@@ -566,16 +566,17 @@ public abstract class BaseCommand implements CommandExecutor {
             String currentFlagName = null;
             final List<String> parameters = new ArrayList<>();
             final List<String> extraParameters = new ArrayList<>();
-            if(!expectedFlags.isEmpty()){
                 for (String arg : arguments) {
-                    if (this.isFlag(arg) || this.isFlagTerminator(arg)) {
-                        if (currentFlagName != null) {
-                            final Flag f = this.createFlag(currentFlagName, parameters.toArray(new String[parameters.size()]));
-                            this.add(f);
+                    if(!expectedFlags.isEmpty()) {
+                        if (this.isFlag(arg) || this.isFlagTerminator(arg)) {
+                            if (currentFlagName != null) {
+                                final Flag f = this.createFlag(currentFlagName, parameters.toArray(new String[parameters.size()]));
+                                this.add(f);
+                            }
+                            parameters.clear();
+                            currentFlagName = this.isFlagTerminator(arg) ? null : this.getFlagName(arg);
+                            continue;
                         }
-                        parameters.clear();
-                        currentFlagName = this.isFlagTerminator(arg) ? null : this.getFlagName(arg);
-                        continue;
                     }
                     arg = arg.replace("\\-", "-");
                     if (currentFlagName != null) parameters.add(arg);
@@ -585,7 +586,7 @@ public abstract class BaseCommand implements CommandExecutor {
                     final Flag f = this.createFlag(currentFlagName, parameters.toArray(new String[parameters.size()]));
                     this.add(f); // last arg can't be neglected
                 }
-            }
+
             this.extraParameters = (String[]) ArrayUtils.addAll(this.extraParameters, extraParameters.toArray(new String[extraParameters.size()]));
         }
     }
