e8d79310581740f849c4d81ca390db26562dcda7
Nathan Blaney <nathan@blaney.xyz>
2023-12-07 21:31:06 +1100

    Remove flag support for /assign, also updates the documentation

--- docs/commands/assign.md
+++ docs/commands/assign.md
@@ -14,15 +14,15 @@ command:
   supports:
     name-completion: false
     time-format: false
-  usage: /assign [command|-#|~]
+  usage: /assign [subcommand]
 layout: command
 title: /assign
 ---
 
-```/assign``` allows for commands to be "assigned" to items. If the command is run while holding an item, clicking with
+```/assign add``` allows for commands to be "assigned" to items. If the command is run while holding an item, clicking with
 that item will execute the command. The ```[command]``` argument should not contain the first ```/``` of a command.
 
-```/assign``` also allows for chat messages to be "assigned" to items, as well. A chat message should be prefixed with
+```/assign add``` also allows for chat messages to be "assigned" to items, as well. A chat message should be prefixed with
 ```c:``` when being used for the ```[command]``` argument.
 
 ```assign.lore_and_display_names``` and ```assign.durability``` can be set to determine whether the command will
@@ -30,8 +30,10 @@ differentiate between items with different display names (and lore) and differen
 
 ### Examples
 
-```/assign jump``` - This will assign ```/jump``` to the item in the player's hand, allowing him to click and teleport
-to the block he is looking at.  
-```/assign c:Hello!``` - This will assign the chat message ```Hello!``` to the item in the player's hand, allowing him
+```/assign add jump``` - This will assign ```/jump``` to the item in the player's hand, allowing them to click and teleport
+to the block he is looking at.
+```/assign add c:Hello!``` - This will assign the chat message ```Hello!``` to the item in the player's hand, allowing them
 to click and say ```Hello!```.
-
+```/assign list``` - This will return the current commands/messages assigned to the item in the player's hand.
+```/assign remove 1``` - This will remove the command/message in slot ```1``` of the item in the player's hand.
+```/assign ``` - This will remove all the commands/messages assigned of the item in the player's hand.
--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/CmdAssign.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/CmdAssign.java
@@ -6,7 +6,9 @@
 package org.royaldev.royalcommands.rcommands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -21,15 +23,13 @@ import org.royaldev.royalcommands.configuration.PlayerConfigurationManager;
 @ReflectCommand
 public class CmdAssign extends TabCommand {
 
-    private final Flag<Integer> removeFlag = new Flag<>(Integer.class, "remove", "r");
-    private final Flag listFlag = new Flag("list", "l");
-
     public CmdAssign(final RoyalCommands instance, final String name) {
-        super(instance, name, true, new Short[]{CompletionType.ANY_COMMAND.getShort()});
-        this.addExpectedFlag(this.removeFlag);
-        this.addExpectedFlag(this.listFlag);
+        super(instance, name, true,new Short[]{CompletionType.LIST.getShort(), CompletionType.ANY_COMMAND.getShort()});
     }
-
+    @Override
+    protected List<String> customList(final CommandSender cs, final Command cmd, final String label, final String[] args, final String arg) {
+        return new ArrayList<>(Arrays.asList("add", "remove", "list"));
+    }
     @Override
     public boolean runCommand(final CommandSender cs, final Command cmd, final String label, final String[] eargs, final CommandArguments ca) {
         if (!(cs instanceof Player)) {
@@ -45,41 +45,43 @@ public class CmdAssign extends TabCommand {
         }
         List<String> cmds = RUtils.getAssignment(hand, pcm);
         if (cmds == null) cmds = new ArrayList<>();
-        if (ca.hasFlag(this.removeFlag)) {
-            Integer toRemove = ca.getFlag(this.removeFlag).getValue();
-            if (toRemove == null) {
-                cs.sendMessage(MessageColor.NEGATIVE + "Please include a valid number to remove.");
-                return true;
-            }
-            if (toRemove <= 0 || toRemove > cmds.size()) {
-                cs.sendMessage(MessageColor.NEGATIVE + "The number specified does not exist!");
-                return true;
-            }
-            toRemove--;
-            cmds.remove((int) toRemove);
-            RUtils.setAssignment(hand, cmds, pcm);
-            cs.sendMessage(MessageColor.POSITIVE + "Removed command " + MessageColor.NEUTRAL + (toRemove + 1) + MessageColor.POSITIVE + ".");
-            return true;
-        } else if (ca.hasFlag(this.listFlag)) {
-            cs.sendMessage(MessageColor.POSITIVE + "Commands on " + MessageColor.NEUTRAL + RUtils.getItemName(hand) + MessageColor.POSITIVE + ":");
-            if (cmds.isEmpty()) {
-                cs.sendMessage("  " + MessageColor.NEUTRAL + "None.");
-                return true;
-            }
-            for (int i = 0; i < cmds.size(); i++) {
-                cs.sendMessage("  " + MessageColor.NEUTRAL + (i + 1) + MessageColor.POSITIVE + ": " + MessageColor.NEUTRAL + cmds.get(i));
-            }
-            return true;
-        }
         if (eargs.length < 1) {
             RUtils.removeAssignment(hand, PlayerConfigurationManager.getConfiguration(p));
             p.sendMessage(MessageColor.POSITIVE + "All commands removed from " + MessageColor.NEUTRAL + RUtils.getFriendlyEnumName(hand.getType()) + MessageColor.POSITIVE + ".");
             return true;
         }
-        cmds.add(RoyalCommands.getFinalArg(eargs, 0));
-        RUtils.setAssignment(hand, cmds, pcm);
-        String message = (RoyalCommands.getFinalArg(eargs, 0).toLowerCase().startsWith("c:")) ? MessageColor.POSITIVE + "Added message " + MessageColor.NEUTRAL + RoyalCommands.getFinalArg(eargs, 0).substring(2) + MessageColor.POSITIVE + " to that item." : MessageColor.POSITIVE + "Added command " + MessageColor.NEUTRAL + "/" + RoyalCommands.getFinalArg(eargs, 0) + MessageColor.POSITIVE + " to that item.";
-        p.sendMessage(message);
-        return true;
+        switch (eargs[0].toLowerCase()) {
+            case "remove":
+                int toRemove = Integer.parseInt(eargs[1]);
+                if (toRemove <= 0 || toRemove > cmds.size()) {
+                    cs.sendMessage(MessageColor.NEGATIVE + "The number specified does not exist!");
+                    return true;
+                }
+                toRemove--;
+                cmds.remove(toRemove);
+                RUtils.setAssignment(hand, cmds, pcm);
+                cs.sendMessage(MessageColor.POSITIVE + "Removed command " + MessageColor.NEUTRAL + (toRemove + 1) + MessageColor.POSITIVE + ".");
+                return true;
+            case "list":
+                cs.sendMessage(MessageColor.POSITIVE + "Commands on " + MessageColor.NEUTRAL + RUtils.getItemName(hand) + MessageColor.POSITIVE + ":");
+                if (cmds.isEmpty()) {
+                    cs.sendMessage("  " + MessageColor.NEUTRAL + "None.");
+                    return true;
+                }
+                for (int i = 0; i < cmds.size(); i++) {
+                    cs.sendMessage("  " + MessageColor.NEUTRAL + (i + 1) + MessageColor.POSITIVE + ": " + MessageColor.NEUTRAL + cmds.get(i));
+                }
+                return true;
+            case "add":
+                cmds.add(eargs[1]);
+                RUtils.setAssignment(hand, cmds, pcm);
+                String message = (eargs[1].toLowerCase().startsWith("c:")) ? MessageColor.POSITIVE + "Added message " + MessageColor.NEUTRAL + eargs[1].replace("c:", "") + MessageColor.POSITIVE + " to that item." : MessageColor.POSITIVE + "Added command " + MessageColor.NEUTRAL + "/" + eargs[1] + MessageColor.POSITIVE + " to that item.";
+                p.sendMessage(message);
+                return true;
+            default:
+                cs.sendMessage(cmd.getDescription());
+                return false;
+        }
+
     }
 }
