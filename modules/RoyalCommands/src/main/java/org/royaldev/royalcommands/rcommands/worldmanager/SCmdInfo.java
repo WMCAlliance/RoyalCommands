b3b278c372f08b03060f679403d4ababf8ec22e8
Matt Gajownik <matt@wizardcm.com>
2025-09-20 00:43:54 +1000

    Updated: Provide significantly more info in /wm info

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/worldmanager/SCmdInfo.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/worldmanager/SCmdInfo.java
@@ -5,27 +5,34 @@
  */
 package org.royaldev.royalcommands.rcommands.worldmanager;
 
+import java.text.DecimalFormat;
+
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.SpawnCategory;
 import org.royaldev.royalcommands.Config;
 import org.royaldev.royalcommands.MessageColor;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
+import org.royaldev.royalcommands.configuration.Configuration;
 import org.royaldev.royalcommands.rcommands.CmdWorldManager;
 import org.royaldev.royalcommands.rcommands.SubCommand;
+import org.royaldev.royalcommands.rcommands.TabCommand;
 
 public class SCmdInfo extends SubCommand<CmdWorldManager> {
 
     public SCmdInfo(final RoyalCommands instance, final CmdWorldManager parent) {
-        super(instance, parent, "info", true, "Displays available world types and environments; if you are a player, displays information about your world.", "<command>", new String[0], new Short[0]);
+        super(instance, parent, "info", true, "Displays available world types and environments; if you are a player, displays information about your world.", "<command>", new String[0], new Short[]{TabCommand.CompletionType.WORLD.getShort()});
     }
 
     @Override
-    public boolean runCommand(final CommandSender cs, final Command cmd, final String label, final String[] eargs, final CommandArguments ca) {
+    public boolean runCommand(final CommandSender cs, final Command cmd, final String label, final String[] args, final CommandArguments ca) {
         if (!Config.useWorldManager) {
             cs.sendMessage(MessageColor.NEGATIVE + "WorldManager is disabled!");
             return true;
@@ -44,13 +51,43 @@ public class SCmdInfo extends SubCommand<CmdWorldManager> {
             envs.append(MessageColor.NEUTRAL).append(e.name()).append(MessageColor.RESET).append(", ");
         }
         cs.sendMessage(envs.substring(0, envs.length() - 4));
-        if (!(cs instanceof Player)) return true;
-        final Player p = (Player) cs;
-        final World w = p.getWorld();
+        World w = null;
+        if (args.length > 0) {
+            w = this.plugin.getServer().getWorld(args[0]);
+        } else if (cs instanceof Player) {
+            final Player p = (Player) cs;
+            w = p.getWorld();
+        }
+        if (w == null) return true;
+        DecimalFormat df = new DecimalFormat("#.##");
+        Location l = w.getSpawnLocation();
+        Configuration cm = RoyalCommands.wm.getConfig();
+        Boolean timeFrozen = cm.getBoolean("worlds." + w.getName() + ".freezetime");
         cs.sendMessage(MessageColor.POSITIVE + "Information on this world:");
         cs.sendMessage(MessageColor.POSITIVE + "  Name: " + MessageColor.NEUTRAL + w.getName());
         cs.sendMessage(MessageColor.POSITIVE + "  Display Name: " + MessageColor.NEUTRAL + RUtils.getMVWorldName(w));
-        cs.sendMessage(MessageColor.POSITIVE + "  Environment: " + MessageColor.NEUTRAL + w.getEnvironment().name());
+        cs.sendMessage(MessageColor.POSITIVE + "  UID: " + MessageColor.NEUTRAL + w.getUID());
+        cs.sendMessage(MessageColor.POSITIVE + "  Game Mode: " + MessageColor.NEUTRAL + cm.getString("worlds." + w.getName() + ".gamemode", "SURVIVAL").toLowerCase());
+        // TODO Seed, but hidden behind a tooltip
+        // cs.sendMessage(MessageColor.POSITIVE + "  Seed: " + MessageColor.NEUTRAL + w.getSeed());
+        cs.sendMessage(MessageColor.POSITIVE + "  Spawn Location: " + MessageColor.NEUTRAL + df.format(l.getX()) + MessageColor.POSITIVE + ", " + MessageColor.NEUTRAL + df.format(l.getY()) + MessageColor.POSITIVE + ", " + MessageColor.NEUTRAL + df.format(l.getZ()) + MessageColor.POSITIVE);
+        cs.sendMessage(MessageColor.POSITIVE + "  Environment: " + MessageColor.NEUTRAL + w.getEnvironment().name().toLowerCase());
+        cs.sendMessage(MessageColor.POSITIVE + "  Generator: " + MessageColor.NEUTRAL + (w.getGenerator() != null ? w.getGenerator().toString() : "default"));
+        cs.sendMessage(MessageColor.POSITIVE + "  Generate Structures: " + MessageColor.NEUTRAL + (w.canGenerateStructures() ? "yes" : "no"));
+        cs.sendMessage(MessageColor.POSITIVE + "  Difficulty: " + MessageColor.NEUTRAL + (w.getDifficulty().toString().toLowerCase()));
+        cs.sendMessage(MessageColor.POSITIVE + "  Animals: " + MessageColor.NEUTRAL + (w.getAllowAnimals() ? "yes" : "no"));
+        cs.sendMessage(MessageColor.POSITIVE + "  Monsters: " + MessageColor.NEUTRAL + (w.getAllowMonsters() ? "yes" : "no"));
+        cs.sendMessage(MessageColor.POSITIVE + "  PVP: " + MessageColor.NEUTRAL + (w.getPVP() ? "yes" : "no"));
+        cs.sendMessage(MessageColor.POSITIVE + "  Time Frozen: " + MessageColor.NEUTRAL + (timeFrozen ? "yes" : "no"));
+        cs.sendMessage(MessageColor.POSITIVE + "  Spawn Limits:");
+        for (SpawnCategory cat : SpawnCategory.values()) {
+            String catName = WordUtils.capitalize(RUtils.getFriendlyEnumName(cat));
+            try {
+                cs.sendMessage(MessageColor.POSITIVE + "    " + catName + ": " + MessageColor.NEUTRAL + w.getSpawnLimit(cat));
+            } catch (IllegalArgumentException ex) {
+                cs.sendMessage(MessageColor.POSITIVE + "    " + catName + ": " + MessageColor.NEUTRAL + "not supported");
+            }
+        }
         return true;
     }
 }
